faef8133863c0142cf5ecd868b4b7328890f920a
Felype Nascimento <felype.nasc@hotmail.com>
2022-09-10 16:23:47 -0300

    icons and images

---
+++ frontend/src/pages/StoryScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="history">Texto da Historia</div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.history {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  justify-content: center;
+  font-family: "Squirk";
+  color: white;
+  font-size: 40px;
+}
+</style>
