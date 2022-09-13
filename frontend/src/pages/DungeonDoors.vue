c6ae663a1729ce3762507b1d5e5a72d184a222c7
gskudo <gskudo@yahoo.com.br>
2022-09-13 09:01:15 -0300

    tailwind adjustment

--- frontend/src/pages/DungeonDoors.vue
+++ frontend/src/pages/DungeonDoors.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="main bg-cover">
-    <div class="header flex flex-row text-white text-xl justify-between">
+    <div
+      class="header flex flex-row text-white text-xl justify-between font-chainwhacks"
+    >
       <img class="logo w-48" src="src/assets/images/logo.svg" />
-      <div class="level flex flex-row text-center align-middle text-7xl">
+      <div
+        class="level flex flex-row text-center align-middle text-7xl font-squirk"
+      >
         <img class="arrows w-10 mr-6" src="src/assets/images/arrowLeft.svg" />
         Level 1
         <img class="arrows w-10 ml-6" src="src/assets/images/arrowRight.svg" />
@@ -13,19 +17,27 @@
       <div class="flex flex-col">
         <img class="door1" src="src/assets/images/1-floor-door-enabled.svg" />
 
-        <label ref="door1" class="text-xl text-white text-center">Door 1</label>
+        <label ref="door1" class="text-xl text-white text-center font-squirk"
+          >Door 1</label
+        >
       </div>
       <div class="flex flex-col">
         <img class="door2" src="src/assets/images/1-floor-door-locked.svg" />
-        <label ref="door2" class="text-xl text-center">Door 2</label>
+        <label ref="door2" class="text-xl text-center font-squirk"
+          >Door 2</label
+        >
       </div>
       <div class="flex flex-col">
         <img class="door3" src="src/assets/images/1-floor-door-locked.svg" />
-        <label ref="door3" class="text-xl text-center">Door 3</label>
+        <label ref="door3" class="text-xl text-center font-squirk"
+          >Door 3</label
+        >
       </div>
       <div class="flex flex-col">
         <img class="door4" src="src/assets/images/1-floor-door-locked.svg" />
-        <label ref="door4" class="text-xl text-center">Door 4</label>
+        <label ref="door4" class="text-xl text-center font-squirk"
+          >Door 4</label
+        >
       </div>
     </div>
   </div>
@@ -34,16 +46,7 @@
 export default {};
 </script>
 <style scoped>
-label {
-  font-family: "Squirk";
-}
 .main {
   background-image: url("src/assets/images/backgroundblue.png");
-}
-.level {
-  font-family: "Squirk";
-}
-.header {
-  font-family: "Chainwhacks";
 }
 </style>
