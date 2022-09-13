a54ca70613198c37b64093d1ee9700acb91ae6e3
gskudo <gskudo@yahoo.com.br>
2022-09-12 22:54:36 -0300

    dungeonsdoors estruturado

--- frontend/src/pages/DungeonDoors.vue
+++ frontend/src/pages/DungeonDoors.vue
@@ -1,57 +1,49 @@
 <template>
-  <div class="main">
-    <div class="header">
-      <img class="logo" src="src/assets/images/logo.svg" />
-      <div class="level">
-        <img class="arrows" src="src/assets/images/arrowLeft.svg" />
+  <div class="main bg-cover">
+    <div class="header flex flex-row text-white text-xl justify-between">
+      <img class="logo w-48" src="src/assets/images/logo.svg" />
+      <div class="level flex flex-row text-center align-middle text-7xl">
+        <img class="arrows w-10 mr-6" src="src/assets/images/arrowLeft.svg" />
         Level 1
-        <img class="arrows" src="src/assets/images/arrowRight.svg" />
+        <img class="arrows w-10 ml-6" src="src/assets/images/arrowRight.svg" />
       </div>
-      <img class="config" src="src/assets/icons/config-icon.svg" />
+      <img class="config w-10" src="src/assets/icons/settings-icon.svg" />
     </div>
-    <div class="doors">
-      <button>sala 1</button>
+    <div class="doors flex flex-row justify-around my-20">
+      <div class="flex flex-col">
+        <img class="door1" src="src/assets/images/1-floor-door-enabled.svg" />
 
-      <button>sala 2</button>
-
-      <button>sala 3</button>
-
-      <button>sala 4</button>
+        <label ref="door1" class="text-xl text-white text-center">Door 1</label>
+      </div>
+      <div class="flex flex-col">
+        <img class="door2" src="src/assets/images/1-floor-door-locked.svg" />
+        <label ref="door2" class="text-xl text-center">Door 2</label>
+      </div>
+      <div class="flex flex-col">
+        <img class="door3" src="src/assets/images/1-floor-door-locked.svg" />
+        <label ref="door3" class="text-xl text-center">Door 3</label>
+      </div>
+      <div class="flex flex-col">
+        <img class="door4" src="src/assets/images/1-floor-door-locked.svg" />
+        <label ref="door4" class="text-xl text-center">Door 4</label>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {};
 </script>
-<style>
+<style scoped>
+label {
+  font-family: "Squirk";
+}
 .main {
   background-image: url("src/assets/images/backgroundblue.png");
-  height: 100vh;
 }
 .level {
   font-family: "Squirk";
-  text-align: center;
-  font-size: 50px;
-  color: white;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
 }
 .header {
-  display: flex;
-  flex-direction: row;
   font-family: "Chainwhacks";
-  font-size: 30px;
-  color: white;
-  justify-content: space-between;
-}
-.logo {
-  width: 18%;
-}
-.config {
-  width: 5%;
-}
-.arrows {
-  width: 10%;
 }
 </style>
