93864fd61a037a20955470ace2e675c98b169ccb
JLucas00 <91480284+JLucas00@users.noreply.github.com>
2022-09-14 02:20:31 -0300

    feat character

---
+++ frontend/src/components/SelectedCharacters.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      default: "light",
+    },
+    shape: {
+      type: String,
+      default: "square",
+    },
+  },
+  computed: {
+    buttonColor() {
+      return this.mode;
+    },
+    buttonShape() {
+      return this.shape;
+    },
+  },
+  methods: {
+    onclick() {
+      this.$emit("onclick");
+    },
+  },
+};
+</script>
+<style></style>
--- frontend/src/pages/TitleScreen.vue
+++ frontend/src/pages/TitleScreen.vue
@@ -1,12 +1,16 @@
 <template>
-  <div @click="openCard" class="noselect flex flex-col justify-around h-full">
+  <div class="noselect flex flex-col justify-around h-full">
     <div class>
       <img src="src/assets/images/logo.svg" class="w-6/12" />
       <h2 class="text-3xl mt-2 tracking-wider font-chainwhacks">
         E O ATAQUE DOS DOGMANÍACOS
       </h2>
     </div>
-    <h3 class="text-2xl font-squirk">CLIQUE PARA CONTINUAR</h3>
+    <div id="continue">
+      <h3 @click="openCard" class="text-2xl font-squirk cursor-pointer">
+        CLIQUE PARA CONTINUAR
+      </h3>
+    </div>
     <CardNickname v-if="showCard" @confirm="confirm" @close="close" />
   </div>
 </template>
@@ -21,6 +25,7 @@ export default {
       showCard: false,
     };
   },
+  created() {},
   methods: {
     openCard() {
       this.showCard = true;
@@ -28,7 +33,7 @@ export default {
     confirm(nickname) {
       console.log(nickname);
       this.showCard = false;
-      console.log(this.showCard);
+      this.$router.push({ path: "/lobby" });
     },
     close() {
       console.log(1);
@@ -38,13 +43,26 @@ export default {
   },
 };
 </script>
-<style>
+<style scoped>
 body {
   background-image: url("src/assets/images/backgroundblue.png");
   display: flex;
   align-items: center;
   justify-content: center;
   color: white;
+}
+#continue {
+  color: white;
+  animation: shine 2s infinite;
+  animation-direction: alternate;
+}
+@keyframes shine {
+  from {
+    color: grey;
+  }
+  to {
+    color: white;
+  }
 }
 img {
   width: 624px;
