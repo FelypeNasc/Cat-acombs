c165768acbe39fbdf2c6b9775b863cf005e5b432
Felype Nascimento <felype.nasc@hotmail.com>
2022-09-25 18:56:22 -0300

    feat: classes raw values

--- backend/src/data/classes.js
+++ backend/src/data/classes.js
@@ -5,21 +5,27 @@ export default classes = {
     description:
       "Resistente e robusto, um guerreiro que faria de tudo por um sachê",
     stats: {
-      hp: 100,
-      attack: 10,
+      baseHp: 100,
+      baseAttack: 10,
       hpPerLevel: 10,
       attackPerLevel: 2,
     },
-    skills: [
-      {
-        id: 1,
-        name: "Ataque Furioso",
-        description: "Um ataque rápido e muito forte, machuca",
-        cooldown: 2,
-        type: "attack",
-        value: 20,
+    actions: {
+      attacks: {
+        normalAttack: { type: "attack", multiplier: 1, accuracy: 100 },
+        strongAttack: { type: "attack", multiplier: 1.5, accuracy: 80 },
       },
-    ],
+      skills: [
+        {
+          id: 1,
+          name: "Ataque Furioso",
+          description: "Um ataque rápido e muito forte, machuca",
+          cooldown: 2,
+          type: "attack",
+          value: 20,
+        },
+      ],
+    },
   },
   ranger: {
     id: 2,
@@ -27,21 +33,27 @@ export default classes = {
     description:
       "Sorrateiro e mortal, tem o andar mais leve dentre todos os gatos",
     stats: {
-      hp: 50,
-      attack: 16,
+      baseHp: 50,
+      baseAttack: 16,
       hpPerLevel: 10,
       attackPerLevel: 2,
     },
-    skills: [
-      {
-        id: 2,
-        name: "Chuva de flechas",
-        description: "Um ataque rápido",
-        cooldown: 4,
-        type: "attack",
-        value: 15,
+    actions: {
+      attacks: {
+        normalAttack: { type: "attack", multiplier: 1, accuracy: 100 },
+        strongAttack: { type: "attack", multiplier: 1.5, accuracy: 80 },
       },
-    ],
+      skills: [
+        {
+          id: 2,
+          name: "Chuva de flechas",
+          description: "Um ataque rápido",
+          cooldown: 4,
+          type: "attack",
+          value: 15,
+        },
+      ],
+    },
   },
   mage: {
     id: 3,
@@ -49,21 +61,27 @@ export default classes = {
     description:
       "Poderoso e implacável, capaz de lançar feitiços devastadores em quem encostar em sua bola de lã",
     stats: {
-      hp: 60,
-      attack: 12,
+      baseHp: 60,
+      baseAttack: 12,
       hpPerLevel: 10,
       attackPerLevel: 2,
     },
-    skills: [
-      {
-        id: 3,
-        name: "Bola de Fogo",
-        description: "Uma bola de fogo que queima, cuidado viu",
-        cooldown: 3,
-        type: "attack",
-        value: 40,
+    actions: {
+      attacks: {
+        normalAttack: { type: "attack", multiplier: 1, accuracy: 100 },
+        strongAttack: { type: "attack", multiplier: 1.5, accuracy: 80 },
       },
-    ],
+      skills: [
+        {
+          id: 3,
+          name: "Bola de Fogo",
+          description: "Uma bola de fogo que queima, cuidado viu",
+          cooldown: 3,
+          type: "attack",
+          value: 40,
+        },
+      ],
+    },
   },
   bard: {
     id: 4,
@@ -71,17 +89,26 @@ export default classes = {
     description:
       "Gracioso, um gato gentil e amigável que faria amizade até com um cachorro",
     stats: {
-      hp: 80,
-      attack: 6,
+      baseHp: 80,
+      baseAttack: 6,
       hpPerLevel: 10,
       attackPerLevel: 2,
     },
     actions: {
       attacks: {
-        normalAttack: { type: "attack", value: attack, accuracy: 100 },
-        strongAttack: { type: "attack", value: attack * 1.5, accuracy: 80 },
+        normalAttack: { type: "attack", multiplier: 1, accuracy: 100 },
+        strongAttack: { type: "attack", multiplier: 1.5, accuracy: 80 },
       },
-      skills: [1],
+      skills: [
+        {
+          id: 4,
+          name: "Música Curativa",
+          description: "Uma música aliviante que faz cura os aliados",
+          cooldown: 2,
+          type: "cure",
+          value: 20,
+        },
+      ],
     },
   },
 };
