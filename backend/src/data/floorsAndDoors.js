8478952d4b36fe3ac9d0f7ffca9057ca572271b6
Augusto Braga <bragaaugusto361@gmail.com>
2022-09-26 00:02:41 -0300

    historia do jogo

--- backend/src/data/floorsAndDoors.js
+++ backend/src/data/floorsAndDoors.js
@@ -1,80 +1,77 @@
-export default floorsAndDoors = {
+export const floorsAndDoors = {
   1: {
     1: {
       type: "battle",
-      boss: {
-        id: 1,
-        name: "Fra-cão",
-      },
+      enemyId: 1,
+      storyText:
+        "Os companheiros começaram a jornada em busca dos sachês perdidos e, de repente, cruzaram com um cachorro malvado, não muito assustador...",
     },
     2: {
       type: "battle",
-      boss: {
-        id: 2,
-        name: "Puguerreiro",
-      },
+      enemyId: 2,
+      storyText:
+        "Ao ser derrotado, Fra-cão acabou falando que o líder dele roubou todo o sachê para ele, indicou um caminho, mas avisou que existiam mais 8 cães guerreiros para enfrentar até chegar ao chefe, logo depois já se depararam com mais um desafio",
     },
     3: {
       type: "rest",
+      storyText:
+        "Ao derrotar o puguerreiro, os gatos amigos estavam exaustos e resolveram armar um acampamento para descansar um pouco, pois ainda tinha muita coisa para vir",
     },
     4: {
       type: "battle",
-      boss: {
-        id: 3,
-        name: "Cãoshin",
-      },
+      enemyId: 3,
+      storyText:
+        "Após o descanso merecido, os gatos continuaram seu caminho, viram seu próximo oponente, um cachoro samurai com cara de louco.",
     },
   },
   2: {
     1: {
       type: "battle",
-      boss: {
-        id: 4,
-        name: "Chow-tchau",
-      },
+      enemyId: 4,
+      storyText:
+        "A luta contra o samurai doido não foi nada fácil, porém conseguiram superá-lo e com isso, encontraram um portal onde iriam encontrar desafios ainda maiores em busca dos sachês perdidos. Passaram pelo portal e logo ja se depararam com outro guerreiro caninio!",
     },
     2: {
       type: "battle",
-      boss: {
-        id: 5,
-        name: "Pastor Sinistrão",
-      },
+      enemyId: 5,
+      storyText:
+        "Essa luta foi difícil, o cão era um maníaco com armas de fogo, mas os amigos gatos o superaram! Logo a frente, os gatos se depararam com uma figura sinistra e assustadora",
     },
     3: {
       type: "rest",
+      storyText:
+        "O cão assustador foi um grande desafio, os gatos ficaram exaustos e necessitam de descanso, montaram novamente o acampamento e ja deitaram",
     },
     4: {
       type: "battle",
-      boss: {
-        id: 6,
-        name: "Akitacalor",
-      },
+      enemyId: 6,
+      storyText:
+        "Ao acordar e continuar a jornada, o grupo se deparou com um guerreiro gracioso, sua espada estava envolvida em chamas, sem dúvida era um cão extremamente poderoso!",
     },
   },
   3: {
     1: {
       type: "battle",
-      boss: {
-        id: 7,
-        name: "Doberserker",
-      },
+      enemyId: 7,
+      storyText:
+        "Mesmo com algumas queimaduras, nossos gatos conseguiram derrotar o cão do fogo, se depararam com mais um portal e ao entrar, deram de cara com um cachorro totalmente mal encarado, muito mais poderoso que qualquer outro desafio!",
     },
     2: {
       type: "battle",
-      boss: {
-        id: 8,
-        name: "Pit Bruto",
-      },
+      enemyId: 8,
+      storyText:
+        "Realmente foi um desafio muito maior do que todos os outros, mas os gatos conseguiram passar e logo a frente, se depararam com mais um desafio, se o cão anterior era mal encarado, esse era o dobro!!!",
     },
     3: {
       type: "rest",
+      storyText:
+        "Essa luta foi a mais insana, o cão mais bruto do mundo tinha acabado de ser derrotado por nossos gatos, que, mais uma vez estavam completamente exaustos, dessa vez nem montaram o acampamento, após a luta caíram e dormiram por lá mesmo.",
     },
     4: {
       type: "battle",
-      boss: {
-        id: 9,
-        name: "loboss",
-      },
+      enemyId: 9,
+      storyText:
+        "Após acordarem, finalmente encontraram quem estavam mais procurando, sim, o chefe dos cães estava lá com os sachês roubados! Ele estava furioso por terem acabado com todos os melhores guerreiros dele, sem falar muito, ele já partiu pra cima dos gatos",
     },
   },
 };
